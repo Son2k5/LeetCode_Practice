c998c44a473828bc798ecb024df0fd97e9bdc434
Haucter <sonct2k3@gmail.com>
2025-08-03 16:43:48 +0700

    Time: 10 ms (97.3%), Space: 73.9 MB (79.17%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -0,0 +1,9 @@
+function containsDuplicate(nums: number[]): boolean {
+     let n = nums.length;
+    let set = new Set();
+    for(let i = 0 ; i< n; i++){
+        if(set.has(nums[i])) return true;
+        set.add(nums[i]);
+    }
+    return false;
+};
