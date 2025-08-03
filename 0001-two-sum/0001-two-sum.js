75a89d0317f9c11fac356fcd641686ca986c541c
Haucter <sonct2k3@gmail.com>
2025-08-03 21:53:25 +0700

    Time: 1 ms (87.77%), Space: 54.7 MB (54.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let n = nums.length;
-    for(let i = 0 ; i < n -1;i++){
-        for(let j = i+1; j < n;j++){
-            if(nums[i] + nums[j] === target ){
-                return [i,j]
-            }
+    const n = nums.length;
+    const map = new Map();
+    for(let i = 0; i < n ; i++){
+        const com = target - nums[i];
+        if(map.has(com)){
+            return [map.get(com),i]
         }
+        map.set(nums[i],i);
     }
     return [];
+
+
+
+
+
+    // let n = nums.length;
+    // for(let i = 0 ; i < n -1;i++){
+    //     for(let j = i+1; j < n;j++){
+    //         if(nums[i] + nums[j] === target ){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+    // return [];
 };
