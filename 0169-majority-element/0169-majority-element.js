97b7080aa349ad965e56934c1192fa4d0b355641
Haucter <sonct2k3@gmail.com>
2025-08-04 07:32:21 +0700

    Time: 4 ms (53.39%), Space: 56 MB (80.68%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = new Map();
+    for(let num of nums){
+        if(map.has(num)){
+            map.set(num, map.get(num) + 1);
+        }
+        else{
+            map.set(num, 1);
+        }
+        if (map.get(num) > nums.length / 2) {
+            return num;
+        }
+    }
+
+};
