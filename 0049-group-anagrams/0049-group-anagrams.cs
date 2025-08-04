789d9750a2fea90b120359ccad3b397fc4ccd0d5
Haucter <sonct2k3@gmail.com>
2025-08-04 07:16:04 +0700

    Time: 20 ms (61.34%), Space: 66.5 MB (78.49%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        var map = new Dictionary<string, List<string>>();
+        foreach(string word in strs){
+            char[] chars= word.ToCharArray();
+            Array.Sort(chars);
+            string key = new string(chars);
+
+            if(!map.ContainsKey(key)) map[key] = new  List<string>();
+            map[key].Add(word);
+        }
+        return map.Values.Select(group => (IList<string>)group).ToList();
+
+    }
+}
