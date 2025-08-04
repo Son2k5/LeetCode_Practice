656e8ffb4603d50a9fefb140e3d086e6575cd08c
Haucter <sonct2k3@gmail.com>
2025-08-04 07:24:03 +0700

    Time: 32 ms (61.86%), Space: 70.1 MB (8.64%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map ={};
+    for(let word of strs){
+        const key = word.split('').sort().join('');
+        if(!map[key]) map[key] = [];
+        map[key].push(word);
+    }
+    return Object.values(map);
+};
