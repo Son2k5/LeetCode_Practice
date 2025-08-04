57a06fbcc5acd4d3bcb0a8085c7a383492b5b552
Haucter <sonct2k3@gmail.com>
2025-08-04 08:26:11 +0700

    Time: 196 ms (22.26%), Space: 80.4 MB (51.7%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.ts
@@ -0,0 +1,21 @@
+function sortArray(nums: number[]): number[] {
+    if (nums.length <= 1) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+    const left = sortArray(nums.slice(0, mid));
+    const right = sortArray(nums.slice(mid));
+
+    return merge(left, right);
+}
+
+function merge(left: number[], right: number[]): number[] {
+    const result: number[] = [];
+    let i = 0, j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) result.push(left[i++]);
+        else result.push(right[j++]);
+    }
+
+    return result.concat(left.slice(i)).concat(right.slice(j));
+}
