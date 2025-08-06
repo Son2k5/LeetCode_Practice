fc14b400420adb68fa955bba002227b0e5f86ea6
Haucter <sonct2k3@gmail.com>
2025-08-06 08:36:19 +0700

    Time: 5 ms (81.1%), Space: 57.6 MB (71.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,16 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function(s) {
-    let maxLen = 0;
+    let seen = new Set();
+    let left = 0, maxLen = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        let set = new Set();
-        for (let j = i; j < s.length; j++) {
-            let c = s.charAt(j);
-            if (set.has(c)) {
-                break;
-            }
-            set.add(c);
-            maxLen = Math.max(maxLen, j - i + 1);
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
         }
+
+        seen.add(s[right]);
+        maxLen = Math.max(maxLen, right - left + 1);
     }
 
     return maxLen;
