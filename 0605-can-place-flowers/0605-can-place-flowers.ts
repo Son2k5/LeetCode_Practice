807b2feefe8beb138f46e2b4ea2ee80ae80bab55
Haucter <sonct2k3@gmail.com>
2025-08-18 13:42:32 +0700

    Time: 1 ms (84.85%), Space: 57.3 MB (86.68%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -0,0 +1,14 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    for (let i = 0; i < flowerbed.length && n > 0; i++) {
+        if (flowerbed[i] === 0) {
+            const prev = (i === 0) ? 0 : flowerbed[i - 1];
+            const next = (i === flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+            
+            if (prev === 0 && next === 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+    }
+    return n <= 0;
+}
