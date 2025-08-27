4b9f19ceb7492aa9cb7b352d2ce89f821af707eb
Haucter <sonct2k3@gmail.com>
2025-08-27 09:30:17 +0700

    Time: 4 ms (68.21%), Space: 55.6 MB (87.86%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxDifference = function(s) {
-    var count = {};
+    var count = Array(26).fill(0);
     var minEven = s.length;
     var maxOdd = 0;
-    for(const c of s) count[c] = (count[c] || 0) + 1;
-    for(var i of Object.values(count)){
+    for(const c of s) count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    for(var i of count){
         if(i == 0) continue;
         if( i % 2 == 1 ) maxOdd = Math.max(maxOdd, i);
         else minEven = Math.min(minEven, i);
