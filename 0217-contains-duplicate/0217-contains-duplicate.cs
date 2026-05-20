420fc06c0974d9a66ecc14076f0e470dcf3cf8e0
Haucter <sonct2k3@gmail.com>
2026-05-20 16:08:08 +0700

    Time: 2 ms (99.89%), Space: 58.3 MB (97.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,13 +1,41 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        HashSet<int> ans = new HashSet<int>();
+        //Solution 1
+        // Su dung sort va kiem tra
         int n = nums.Length;
-        for(int i =0; i < n; i++){
-            if(ans.Contains(nums[i])){
-                return true;
-            }
-            ans.Add(nums[i]);
+        for(int i = 0 ; i< n; i++){
+            int temp = nums[i];
+            int j = i - 1;
+            while(j >= 0 && nums[j] > temp){
+                nums[j+1] = nums[j];
+                nums[j] = temp;
+                j--;
+            
+        }
+        nums[j + 1] = temp;
+
+            if (j >= 0 && nums[j] == nums[j + 1])
+        return true;
         }
         return false;
+       
+       
+       
+       
+       
+       
+       
+       
+        // Solution 2
+        //su dung hashset
+        // HashSet<int> ans = new HashSet<int>();
+        // int n = nums.Length;
+        // for(int i =0; i < n; i++){
+        //     if(ans.Contains(nums[i])){
+        //         return true;
+        //     }
+        //     ans.Add(nums[i]);
+        // }
+        // return false;
     }
 }
